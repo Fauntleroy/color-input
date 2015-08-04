b36c69a650744d8c35f98c14e252ba41f6e25b6e
Timothy Kempf <the.kaempfer@gmail.com>
2015-08-03 21:54:16 -0700

    basic color setting functionality

--- src/index.jsx
+++ src/index.jsx
@@ -1,1 +1,35 @@
-var indexStyles = require('./index.css');
+import './index.css';
+
+import React from 'react';
+
+var App = React.createClass({
+    getInitialState: function(){
+        return {};
+    },
+    render: function(){
+        var displayStyle = {
+            backgroundColor: this.state.color
+        };
+        return (
+            <div className="app">
+                <input
+                    className="color"
+                    type="text"
+                    placeholder="Enter a color or code"
+                    value={this.state.color}
+                    onChange={this._onInputChange}
+                />
+                <div className="color-display" style={displayStyle}></div>
+            </div>
+        );
+    },
+    _onInputChange: function( event ){
+        this.setState({
+            color: event.target.value
+        });
+    }
+});
+
+document.addEventListener( 'DOMContentLoaded', function(){
+    React.render( <App />, document.body );
+});
